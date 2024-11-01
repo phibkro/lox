ba1fd726a1fe959403e38f07caa549cedd7e5365
phibkro <71797726+phibkro@users.noreply.github.com>
2024-11-02 00:40:48 +0100

    Update generated package name

--- jlox/java/src/tool/GenerateAst.java
+++ jlox/java/src/tool/GenerateAst.java
@@ -26,7 +26,7 @@ public class GenerateAst {
         String path = outputDir + "/" + baseName + ".java";
         PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-        writer.println("package com.craftinginterpreters.lox;");
+        writer.println("package jlox;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
