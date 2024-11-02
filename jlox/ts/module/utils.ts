f8e9d458961d2329ad413017a79be406a7e1ab4c
phibkro <71797726+phibkro@users.noreply.github.com>
2024-11-02 02:13:51 +0100

    Assert return type

--- jlox/ts/module/utils.ts
+++ jlox/ts/module/utils.ts
@@ -1,6 +1,10 @@
 export function getKeyFromValue<K extends string | number | symbol, V>(
   object: Record<K, V>,
   value: V,
-) {
-  return Object.keys(object).find((key) => object[key as K] === value);
+): K | undefined {
+  return Object.keys(object).find(
+    // Assert key type as key of Record<K, V> is always of type K
+    (key) => object[key as K] === value,
+    // Assert return type as find() on Record<K, V> always returns K or undefined
+  ) as K | undefined;
 }
