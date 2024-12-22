c017ecad75147f45005c702c776c86fe05d0100a
phibkro <71797726+phibkro@users.noreply.github.com>
2024-12-22 15:17:45 +0100

    dump work on tokenization

--- jlox/rust/src/lib.rs
+++ jlox/rust/src/lib.rs
@@ -34,6 +34,165 @@ pub fn run_file(path: &PathBuf) -> io::Result<()> {
     Ok(())
 }
 
-fn run(command: &str) {
-    println!("{:?}", command);
+fn run(code: &str) {
+    let tokens = tokenize();
+    println!("{:?}", code);
+}
+
+/* Token */
+
+pub fn tokenize() -> Vec<TokenType> {
+    todo!()
+}
+
+pub enum Literal {
+    Identifier(String),
+    String(String),
+    Number(String),
+}
+pub enum Operator {
+    Minus,
+    Plus,
+    Slash,
+    Star,
+    Bang,
+}
+pub enum TokenType {
+    /* Single-character tokens */
+    // Regions
+    ParenOpen,
+    ParenClosed,
+    BraceOpen,
+    BraceClosed,
+    // Separators
+    Comma,
+    Semicolon,
+    Dot,
+    // Math Operators
+    Minus,
+    Plus,
+    Slash,
+    Star,
+
+    /* One or two character tokens */
+    // Negation
+    Bang,
+    // Assignment
+    Eq,
+    // Comparison
+    BangEq,
+    EqEq,
+    Greater,
+    GreaterEq,
+    Less,
+    LessEq,
+
+    // Literals.
+    Identifier(String),
+    String(String),
+    Number(String),
+
+    /* Keywords */
+    // Boolean
+    True,
+    False,
+    // Control Flow
+    If,
+    Else,
+    Return,
+    // OOP
+    Class,
+    This,
+    Super,
+    // Loop
+    While,
+    For,
+    //
+    And,
+    Or,
+    //
+    Fun,
+    Var,
+    //
+    Nil,
+    // IO
+    Print,
+    //
+    Eof,
+}
+impl TokenType {
+    fn to_string(&self) -> &str {
+        match self {
+            TokenType::ParenOpen => "(",
+            TokenType::ParenClosed => ")",
+            TokenType::BraceOpen => "{",
+            TokenType::BraceClosed => "}",
+            TokenType::Comma => ",",
+            TokenType::Dot => ".",
+            TokenType::Minus => "-",
+            TokenType::Plus => "+",
+            TokenType::Semicolon => ";",
+            TokenType::Slash => "/",
+            TokenType::Star => "*",
+            TokenType::Bang => "!",
+            TokenType::BangEq => "!=",
+            TokenType::Eq => "=",
+            TokenType::EqEq => "==",
+            TokenType::Greater => "<",
+            TokenType::GreaterEq => "<=",
+            TokenType::Less => ">",
+            TokenType::LessEq => ">=",
+            TokenType::Identifier(i) => i,
+            TokenType::String(s) => s,
+            TokenType::Number(n) => n,
+            TokenType::And => "and",
+            TokenType::Class => "class",
+            TokenType::Else => "else",
+            TokenType::False => "false",
+            TokenType::Fun => "fun",
+            TokenType::For => "for",
+            TokenType::If => "if",
+            TokenType::Nil => "nil",
+            TokenType::Or => "or",
+            TokenType::Print => "print",
+            TokenType::Return => "return",
+            TokenType::Super => "super",
+            TokenType::This => "this",
+            TokenType::True => "true",
+            TokenType::Var => "var",
+            TokenType::While => "while",
+            TokenType::Eof => todo!(),
+        }
+    }
+}
+
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    line: u32,
+}
+impl Token {
+    fn to_string(&self) -> String {
+        return self.token_type.to_string().to_owned()
+            + " "
+            + &self.lexeme.to_owned()
+            + " "
+            + self.literal();
+    }
+    fn literal(&self) -> &str {
+        self.token_type.to_string()
+    }
+}
+
+/* Expression */
+
+enum Primary {
+    String(String),
+    Number(i32),
+    Bool(Boolean),
+    Expression(Literal),
+}
+enum Boolean {
+    True,
+    False,
 }
