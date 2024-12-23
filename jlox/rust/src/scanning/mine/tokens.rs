15188e9784b29e7f494edd6e3ed460553ff4e017
phibkro <71797726+phibkro@users.noreply.github.com>
2024-12-23 03:03:16 +0100

    rename tokentypes

--- jlox/rust/src/scanning/mine/tokens.rs
+++ jlox/rust/src/scanning/mine/tokens.rs
@@ -5,12 +5,15 @@ pub struct Token {
     pub line: usize,
 }
 
+/* Single-character tokens */
+
+// Regions
 pub const PAREN_OPEN: TokenType = TokenType::ParenOpen("(");
 pub const PAREN_CLOSED: TokenType = TokenType::ParenClosed(")");
 
 // Block
-pub const BRACE_OPEN: TokenType = TokenType::BraceOpen("{");
-pub const BRACE_CLOSED: TokenType = TokenType::BraceClosed("}");
+pub const BRACE_OPEN: TokenType = TokenType::BlockOpen("{");
+pub const BRACE_CLOSED: TokenType = TokenType::BlockClosed("}");
 
 // Separators
 pub const COMMA: TokenType = TokenType::Comma(",");
@@ -18,24 +21,33 @@ pub const SEMICOLON: TokenType = TokenType::Semicolon(";");
 pub const DOT: TokenType = TokenType::Dot(".");
 
 // Math Operators
-pub const MINUS: TokenType = TokenType::Minus("-");
-pub const PLUS: TokenType = TokenType::Plus("+");
-pub const SLASH: TokenType = TokenType::Slash("/");
-pub const STAR: TokenType = TokenType::Star("*");
+pub const MINUS: TokenType = TokenType::Subtraction("-");
+pub const PLUS: TokenType = TokenType::Addition("+");
+pub const SLASH: TokenType = TokenType::Division("/");
+pub const STAR: TokenType = TokenType::Multiplication("*");
 
 // Negation
-pub const BANG: TokenType = TokenType::Bang("!");
+pub const BANG: TokenType = TokenType::Not("!");
 
 // Assignment
-pub const EQ: TokenType = TokenType::Eq("=");
+pub const EQ: TokenType = TokenType::Assignment("=");
 
 // Comparison
-pub const BANG_EQ: TokenType = TokenType::BangEq("!=");
-pub const EQ_EQ: TokenType = TokenType::EqEq("==");
-pub const GREATER: TokenType = TokenType::Greater(">");
-pub const GREATER_EQ: TokenType = TokenType::GreaterEq(">=");
-pub const LESS: TokenType = TokenType::Less("<");
-pub const LESS_EQ: TokenType = TokenType::LessEq("<=");
+pub const GREATER: TokenType = TokenType::GreaterThan(">");
+pub const LESS: TokenType = TokenType::LessThan("<");
+
+/* Two character tokens */
+
+pub const BANG_EQ: TokenType = TokenType::NotEqual("!=");
+pub const EQ_EQ: TokenType = TokenType::DoubleEqual("==");
+pub const GREATER_EQ: TokenType = TokenType::GreaterThanOrEqual(">=");
+pub const LESS_EQ: TokenType = TokenType::LessThanOrEqual("<=");
+
+/* Keywords */
+
+// Loop
+pub const WHILE: TokenType = TokenType::While("while");
+pub const FOR: TokenType = TokenType::For("for");
 
 // Boolean
 pub const TRUE: TokenType = TokenType::True("true");
@@ -46,22 +58,18 @@ pub const IF: TokenType = TokenType::If("if");
 pub const ELSE: TokenType = TokenType::Else("else");
 pub const RETURN: TokenType = TokenType::Return("return");
 
-// OOP
-pub const CLASS: TokenType = TokenType::Class("class");
-pub const THIS: TokenType = TokenType::This("this");
-pub const SUPER: TokenType = TokenType::Super("super");
-
-// Loop
-pub const WHILE: TokenType = TokenType::While("while");
-pub const FOR: TokenType = TokenType::For("for");
-
 // Logical
 pub const AND: TokenType = TokenType::And("and");
 pub const OR: TokenType = TokenType::Or("or");
 
 // Declaration
-pub const FUN: TokenType = TokenType::Fun("fun");
-pub const VAR: TokenType = TokenType::Var("var");
+pub const VAR: TokenType = TokenType::Variable("var");
+pub const FUN: TokenType = TokenType::Function("fun");
+
+// OOP
+pub const CLASS: TokenType = TokenType::Class("class");
+pub const THIS: TokenType = TokenType::This("this");
+pub const SUPER: TokenType = TokenType::Super("super");
 
 // Nil
 pub const NIL: TokenType = TokenType::Nil("nil");
@@ -71,65 +79,85 @@ pub const PRINT: TokenType = TokenType::Print("print");
 
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
-    /* Single-character tokens */
+    /* Primitives */
+
     // Regions
     ParenOpen(&'static str),
     ParenClosed(&'static str),
-    BraceOpen(&'static str),
-    BraceClosed(&'static str),
+
+    // Block
+    BlockOpen(&'static str),
+    BlockClosed(&'static str),
+
     // Separators
     Comma(&'static str),
     Semicolon(&'static str),
     Dot(&'static str),
-    // Math Operators
-    Minus(&'static str),
-    Plus(&'static str),
-    Slash(&'static str),
-    Star(&'static str),
 
-    /* One or two character tokens */
-    // Negation
-    Bang(&'static str),
-    // Assignment
-    Eq(&'static str),
+    // Math Operators
+    Subtraction(&'static str),
+    Addition(&'static str),
+    Division(&'static str),
+    Multiplication(&'static str),
+
     // Comparison
-    BangEq(&'static str),
-    EqEq(&'static str),
-    Greater(&'static str),
-    GreaterEq(&'static str),
-    Less(&'static str),
-    LessEq(&'static str),
+    NotEqual(&'static str),
+    DoubleEqual(&'static str),
+    GreaterThan(&'static str),
+    GreaterThanOrEqual(&'static str),
+    LessThan(&'static str),
+    LessThanOrEqual(&'static str),
+
+    /* Types */
 
     // Literals.
     Identifier(String),
     String(String),
     Number(String),
 
-    /* Keywords */
     // Boolean
     True(&'static str),
     False(&'static str),
-    // Control Flow
+    
+    // Nil
+    Nil(&'static str),
+
+    /* Control Flow */
+
+    Return(&'static str),
+
+    // Conditional
     If(&'static str),
     Else(&'static str),
-    Return(&'static str),
-    // OOP
+
+    // Iteration
+    While(&'static str),
+    For(&'static str),
+    
+    // Logical
+    Not(&'static str),
+    And(&'static str),
+    Or(&'static str),
+
+    /* */
+
+    // Assignment
+    Assignment(&'static str),
+
+    // Declaration
+    Variable(&'static str),
+    Function(&'static str),
+    
+    // Object Orientation
     Class(&'static str),
     This(&'static str),
     Super(&'static str),
-    // Loop
-    While(&'static str),
-    For(&'static str),
-    //
-    And(&'static str),
-    Or(&'static str),
-    //
-    Fun(&'static str),
-    Var(&'static str),
-    //
-    Nil(&'static str),
+    
+    /* Extras */
+
     // IO
     Print(&'static str),
-    //
+
+    // End of file
     Eof,
 }
